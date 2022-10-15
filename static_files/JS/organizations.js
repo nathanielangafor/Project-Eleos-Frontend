532601c8d75863f6b9de5df5e17c0dbc42c8a67f
Simon Cheng <loopingthroughlife@gmail.com>
2022-10-15 21:05:49 +0000

    login things

--- static_files/JS/organizations.js
+++ static_files/JS/organizations.js
@@ -2,7 +2,11 @@ async function loadData() {
     var arr = await (await fetch('https://port-7788.ms.binary-person.dev/api/projects')).json();
     var user = await (await fetch('https://port-7788.ms.binary-person.dev/api/user')).json();
     document.getElementById('user-greeting').textContent = 'Welcome ' + user.displayName;
-    document.getElementById('');
+    document.getElementById('logout-btn').onclick = function() {
+        firebase.auth().signOut();
+    };
+    console.log(user);
+    document.querySelector('.enav-right').style.display = 'block';
 
     var data = ''
     arr.forEach(element => {
@@ -11,21 +15,44 @@ async function loadData() {
         sym.forEach(ell => {
             generatedSym = generatedSym + ell[0]
         })
-        data = data + `<div class="card mb-3"> <div class="card-body"> <div class="d-flex flex-column flex-lg-row"> <span class="avatar avatar-text rounded-3 me-4 bg-warning mb-2">${generatedSym.toUpperCase()}</span> <div class="row flex-fill"> <div class="col-sm-5"> <h4 class="h5">${element['name']}</h4> <span class="badge bg-secondary">${element['location']}</span> </div> <div class="col-sm-4 py-2"> <span class="badge bg-secondary">${element['tags'][0]}</span>  </div> <div class="col-sm-3 text-lg-end"> <a href="#" onClick="" class="btn btn-primary stretched-link">Donate Resources!</a> </div> </div> </div> </div> </div>`
+        console.log(element);
+        data = data
+            + `
+        <div class="card mb-3">
+                <div class="card-body">
+                    <div class="d-flex flex-column flex-lg-row">
+                    <span class="avatar avatar-text rounded-3 me-4 bg-warning mb-2">${generatedSym.toUpperCase()}</span>
+                    <div class="row flex-fill">
+                        <div class="col-sm-5">
+                            <h4 class="h5">${element['name']}</h4>
+                            <span class="badge bg-secondary">${element['location']}</span>
+                        </div>
+                        <div class="col-sm-4 py-2">
+                            <span class="badge bg-secondary">${element['tags'][0]}</span>
+                        </div>
+                        <div class="col-sm-3 text-lg-end">
+                            <a href="#" onClick="donate('${element.id}')" class="btn btn-primary stretched-link">${user.projects.includes(element.id) ? 'Currently donating' : 'Donate resources!'}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
     });
     document.getElementById("orgs").innerHTML = data;
 }
 
+window.donate = function donate(projectId) {
+    console.log(projectId);
+}
+
 window.onload = function () {
-    if (firebase.auth().currentUser) {
-        loadData();
-    } else {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                loadData();
-            }
-        });
-    }
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            loadData();
+        } else {
+            window.location.href = 'login.html?redirect=' + encodeURIComponent(location.href);
+        }
+    });
 };
 
 
