b33e7c3ff8ac0450e9a40db69c6c7a9237c34fa0
Nathaniel Angafor <nangafor@u.rochester.edu>
2022-10-15 12:06:00 -1000

    background script tingzzz

---
+++ static_files/JS/backgroundScript.js
@@ -0,0 +1,8 @@
+
+async function checkIn() {
+    // make http request to server for tasks under one of the two orgs we are subscribed to
+    // if task exists, return javascript code as a string
+    var generatedFunction = await eval("console.log('Hello world')")
+}
+
+setInterval(checkIn(), 10000)
